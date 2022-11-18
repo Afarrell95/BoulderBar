239adc7043c195cce2571a9695b01f50d95fd86e
Allison <farrella95@gmail.com>
2022-11-18 08:59:40 -0500

    finished for now

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -1,25 +1,42 @@
 import React from "react";
 import "./About.css";
+import picture from "./img/headshot2.jpg";
 
 const styles = {
+  pic: {
+    width: "200px",
+    borderRadius: "50%",
+    border: "double rgba(0,31,250,.5) 10px",
+    transform: "translateY(-9%)",
+  },
+
   body: {
-    background: "rgba(255,255,255,.25)",
+    background: "rgba(255,255,255,.4)",
     borderRadius: "5px",
     display: "grid",
     placeItems: "center",
     margin: "auto",
-    width: "50%",
-    transform: "translateY(100%)",
+    width: "60%",
+    transform: "translateY(20%)",
     fontFamily: "poppins",
   },
   heading: {
-    background: "rgba(255,255,255,.45)",
+    background: "rgba(255,255,255,.6)",
+    display: "grid",
+    placeItems: "center",
     borderRadius: "5px 5px 0px 0px",
-    color: "rgb(19, 38, 61)",
+    color: "navy",
     fontSize: "20px",
     width: "100%",
-    height: "15vmin",
-    textalign: "center",
+    height: "20vmin",
+  },
+  para: {
+    color: "navy",
+    textIndent: "50px",
+    paddingLeft: "20px",
+    paddingRight: "20px",
+    width: "70%",
+    letterSpacing: "1.5px",
   },
 };
 
@@ -32,18 +49,22 @@ export default function About() {
         </header>
 
         <div className="head-shot">
-          <img src="./img/headshot2.jpg" alt="Allison's headshot image" />
+          <img
+            style={styles.pic}
+            src={picture}
+            alt="Allison's headshot image"
+          />
         </div>
-        <section>
+        <section style={styles.para}>
           <p>
             Former trauma hospital worker of 5 years turned Frontend Developer.
             A Savannah, GA transplant born and raised in South Jersey.
           </p>
           <br />
           <p>
-            When I'm not in the gym or with my 3 dogs, I'm on my computer coding
-            responsive and accessable websites and furthuring my Frontend
-            skillset.
+            When I'm not in the gym or with my cat and 3 dogs, I'm on my
+            computer coding responsive and accessable websites and furthuring my
+            Frontend skillset.
           </p>
           <br />
         </section>
--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -1,15 +1,65 @@
 import React from "react";
 import "./Contact.css";
 
+const styles = {
+  body: {
+    background: "rgba(255,255,255,.4)",
+    borderRadius: "5px",
+    display: "grid",
+    placeItems: "center",
+    margin: "auto",
+    width: "50%",
+    fontFamily: "poppins",
+  },
+  heading: {
+    background: "rgba(255,255,255,.6)",
+    borderRadius: "5px 5px 0px 0px",
+    color: "navy",
+    fontSize: "20px",
+    width: "100%",
+    height: "15vmin",
+    textalign: "center",
+  },
+  spans: {
+    color: "navy",
+    fontSize: "20px",
+    letterSpacing: "1.5px",
+    lineHeight: "2.5",
+    display: "flex",
+    flexDirection: "column",
+  },
+};
+
 export default function Contact() {
   return (
     <div className="contain">
-      <body>
-        <h1>Want to get in touch?</h1>
+      <body style={styles.body}>
+        <header style={styles.heading}>
+          <h1>Want to get in touch?</h1>
+        </header>
         <section>
-          Email: farrella95@gmail.com Phone: (856)-562-5881 LinkedIn:
-          https://www.linkedin.com/in/allison-farrell-6815a0214/ GitHub:
-          https://github.com/Afarrell95
+          <span style={styles.spans}> Email: farrella95@gmail.com</span>
+
+          <span style={styles.spans}>Phone: (856)-562-5881 </span>
+          <span style={styles.spans}>
+            <a
+              target="_blank"
+              href="https://www.linkedin.com/in/allison-farrell-6815a0214/"
+            >
+              LinkedIn
+            </a>
+          </span>
+          <span style={styles.spans}>
+            <a target="_blank" href="https://twitter.com/Allison_WebDev">
+              Twitter... until Elon kills it
+            </a>
+          </span>
+          <span style={styles.spans}>
+            <a target="_blank" href="https://github.com/Afarrell95">
+              {" "}
+              GitHub
+            </a>
+          </span>
         </section>
       </body>
     </div>
