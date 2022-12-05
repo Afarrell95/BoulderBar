838153c2328d3daa336556feb2fc218142eb8a23
Allison01~ <farrella95@gmail.com>
2022-12-05 09:48:57 -0500

    responsiveness

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -2,61 +2,24 @@ import React from "react";
 import "./About.css";
 import picture from "./img/headshot2.jpg";
 
-const styles = {
-  pic: {
-    width: "200px",
-    borderRadius: "50%",
-    border: "double #150445 10px",
-    transform: "translateY(-9%)",
-  },
 
-  body: {
-    background: "rgba(225, 230, 211, 1)",
-    borderRadius: "5px",
-    display: "grid",
-    placeItems: "center",
-    margin: "auto",
-    width: "60%",
-    transform: "translateY(5%)",
-    fontFamily: "poppins",
-    boxShadow: "0px 0px 5px 1px rgba(116, 117, 128, 0.66)",
-  },
-  heading: {
-    background: "rgba(255,255,255,.8)",
-    display: "grid",
-    placeItems: "center",
-    borderRadius: "5px 5px 0px 0px",
-    color: "#150445",
-    fontSize: "20px",
-    width: "100%",
-    height: "20vmin",
-  },
-  para: {
-    color: "#150445",
-    textIndent: "50px",
-    paddingLeft: "20px",
-    paddingRight: "20px",
-    width: "70%",
-    letterSpacing: "1.5px",
-  },
-};
 
 export default function About() {
   return (
     <div>
-      <body style={styles.body}>
-        <header style={styles.heading}>
+      <body className="body">
+        <header className="heading">
           <h1>Hello! My name is Allison.</h1>
         </header>
 
         <div className="head-shot">
-          <img
-            style={styles.pic}
+          <img className="head-img"
+            
             src={picture}
             alt="Allison's headshot image"
           />
         </div>
-        <section style={styles.para}>
+        <section className="para">
           <p>
             Former trauma hospital worker of 5 years turned Frontend Developer.
             A Savannah, GA transplant born and raised in South Jersey.
--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -1,48 +1,19 @@
 import React from "react";
 import "./Contact.css";
 
-const styles = {
-  body: {
-    background: "rgba(225, 230, 211, 1)",
-    borderRadius: "5px",
-    display: "grid",
-    placeItems: "center",
-    margin: "auto",
-    width: "50%",
-    fontFamily: "poppins",
-    boxShadow: "0px 0px 5px 1px rgba(116, 117, 128, 0.66)",
-  },
-  heading: {
-    background: "rgba(255,255,255,.8)",
-    borderRadius: "5px 5px 0px 0px",
-    color: "#150445",
-    fontSize: "20px",
-    width: "100%",
-    height: "15vmin",
-    textalign: "center",
-  },
-  spans: {
-    color: "#150445",
-    fontSize: "20px",
-    letterSpacing: "1.5px",
-    lineHeight: "2.5",
-    display: "flex",
-    flexDirection: "column",
-  },
-};
 
 export default function Contact() {
   return (
     <div className="contain">
-      <body style={styles.body}>
-        <header style={styles.heading}>
+      <body className="body">
+        <header className="heading">
           <h1>Want to get in touch?</h1>
         </header>
         <section>
-          <span style={styles.spans}> Email: farrella95@gmail.com</span>
+          <span className="spans"> Email: farrella95@gmail.com</span>
 
-          <span style={styles.spans}>Phone: (856)-562-5881 </span>
-          <span style={styles.spans}>
+          <span className="spans">Phone: (856)-562-5881 </span>
+          <span className="spans">
             <a
               target="_blank"
               href="https://www.linkedin.com/in/allison-farrell-6815a0214/"
@@ -50,12 +21,12 @@ export default function Contact() {
               LinkedIn
             </a>
           </span>
-          <span style={styles.spans}>
+          <span className="spans">
             <a target="_blank" href="https://twitter.com/Allison_WebDev">
               Twitter
             </a>
           </span>
-          <span style={styles.spans}>
+          <span className="spans">
             <a target="_blank" href="https://github.com/Afarrell95">
               {" "}
               GitHub
