b3c840633fa22de689066c394e2da7af06b1927d
Allison <farrella95@gmail.com>
2022-12-14 10:48:13 -0500

    nav and foot

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,22 +4,228 @@ import About from "./pages/About";
 import Contact from "./pages/Contact";
 import "./Home.css";
 import React, { useState, useEffect } from "react";
+import {
+  MDBFooter,
+  MDBContainer,
+  MDBRow,
+  MDBCol,
+  MDBIcon,
+} from "mdb-react-ui-kit";
 
 function Home({ currentPage, handlePageChange }) {
-  const [style, setStyle] = useState("card-group");
-
-  const changeStyle = () => {
-    setStyle("hidden");
-  };
-  const [styleHome, setStyleHome] = useState("hidden");
-
-  const changeStyleHome = () => {
-    setStyle("card-group");
-  };
   return (
     <div>
+      <head>
+        <link
+          rel="stylesheet"
+          href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css"
+          integrity="sha384-9gVQ4dYFwwWSjIDZnLEWnxCjeSWFphJiwGPXr1jddIhOegiu1FwO5qRGvFXOdJZ4"
+          crossorigin="anonymous"
+        />
+      </head>
+
       <body className="home-body">
-       
+        <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+          <a className="navbar-brand" href="#">
+            Navbar
+          </a>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-toggle="collapse"
+            data-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav mr-auto">
+              <li className="nav-item active">
+                <a className="nav-link" href="#">
+                  Home <span class="sr-only">(current)</span>
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#">
+                  Link
+                </a>
+              </li>
+              <li className="nav-item dropdown">
+                <a
+                  className="nav-link dropdown-toggle"
+                  href="#"
+                  id="navbarDropdown"
+                  role="button"
+                  data-toggle="dropdown"
+                  aria-haspopup="true"
+                  aria-expanded="false"
+                >
+                  Dropdown
+                </a>
+                <div className="dropdown-menu" aria-labelledby="navbarDropdown">
+                  <a className="dropdown-item" href="#">
+                    Action
+                  </a>
+                  <a className="dropdown-item" href="#">
+                    Another action
+                  </a>
+                  <div className="dropdown-divider"></div>
+                  <a className="dropdown-item" href="#">
+                    Something else here
+                  </a>
+                </div>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#">
+                  Disabled
+                </a>
+              </li>
+            </ul>
+            <form className="form-inline my-2 my-lg-0">
+              <input
+                className="form-control mr-sm-2"
+                type="search"
+                placeholder="Search Products"
+                aria-label="Search"
+              />
+              <button
+                className="btn btn-outline-info my-2 my-sm-0"
+                type="submit"
+              >
+                Search
+              </button>
+            </form>
+          </div>
+        </nav>
+
+        <MDBFooter
+          bgColor="light"
+          className="text-center text-lg-start text-muted"
+        >
+          <section className="d-flex justify-content-center justify-content-lg-between p-4 border-bottom">
+            <div className="me-5 d-none d-lg-block">
+              <span>Get connected with us on social networks:</span>
+            </div>
+
+            <div>
+              <a href="" className="me-4 text-reset">
+                <MDBIcon fab icon="facebook-f" />
+              </a>
+              <a href="" className="me-4 text-reset">
+                <MDBIcon fab icon="twitter" />
+              </a>
+              <a href="" className="me-4 text-reset">
+                <MDBIcon fab icon="google" />
+              </a>
+              <a href="" className="me-4 text-reset">
+                <MDBIcon fab icon="instagram" />
+              </a>
+              <a href="" className="me-4 text-reset">
+                <MDBIcon fab icon="linkedin" />
+              </a>
+              <a href="" className="me-4 text-reset">
+                <MDBIcon fab icon="github" />
+              </a>
+            </div>
+          </section>
+
+          <section className="">
+            <MDBContainer className="text-center text-md-start mt-5">
+              <MDBRow className="mt-3">
+                <MDBCol md="3" lg="4" xl="3" className="mx-auto mb-4">
+                  <h6 className="text-uppercase fw-bold mb-4">
+                    <MDBIcon icon="gem" className="me-3" />
+                    Company name
+                  </h6>
+                  <p>
+                    Here you can use rows and columns to organize your footer
+                    content. Lorem ipsum dolor sit amet, consectetur adipisicing
+                    elit.
+                  </p>
+                </MDBCol>
+
+                <MDBCol md="2" lg="2" xl="2" className="mx-auto mb-4">
+                  <h6 className="text-uppercase fw-bold mb-4">Products</h6>
+                  <p>
+                    <a href="#!" className="text-reset">
+                      Angular
+                    </a>
+                  </p>
+                  <p>
+                    <a href="#!" className="text-reset">
+                      React
+                    </a>
+                  </p>
+                  <p>
+                    <a href="#!" className="text-reset">
+                      Vue
+                    </a>
+                  </p>
+                  <p>
+                    <a href="#!" className="text-reset">
+                      Laravel
+                    </a>
+                  </p>
+                </MDBCol>
+
+                <MDBCol md="3" lg="2" xl="2" className="mx-auto mb-4">
+                  <h6 className="text-uppercase fw-bold mb-4">Useful links</h6>
+                  <p>
+                    <a href="#!" className="text-reset">
+                      Pricing
+                    </a>
+                  </p>
+                  <p>
+                    <a href="#!" className="text-reset">
+                      Settings
+                    </a>
+                  </p>
+                  <p>
+                    <a href="#!" className="text-reset">
+                      Orders
+                    </a>
+                  </p>
+                  <p>
+                    <a href="#!" className="text-reset">
+                      Help
+                    </a>
+                  </p>
+                </MDBCol>
+
+                <MDBCol md="4" lg="3" xl="3" className="mx-auto mb-md-0 mb-4">
+                  <h6 className="text-uppercase fw-bold mb-4">Contact</h6>
+                  <p>
+                    <MDBIcon icon="home" className="me-2" />
+                    New York, NY 10012, US
+                  </p>
+                  <p>
+                    <MDBIcon icon="envelope" className="me-3" />
+                    info@example.com
+                  </p>
+                  <p>
+                    <MDBIcon icon="phone" className="me-3" /> + 01 234 567 88
+                  </p>
+                  <p>
+                    <MDBIcon icon="print" className="me-3" /> + 01 234 567 89
+                  </p>
+                </MDBCol>
+              </MDBRow>
+            </MDBContainer>
+          </section>
+
+          <div
+            className="text-center p-4"
+            style={{ backgroundColor: "rgba(0, 0, 0, 0.05)" }}
+          >
+            © 2021 Copyright:
+            <a className="text-reset fw-bold" href="https://mdbootstrap.com/">
+              MDBootstrap.com
+            </a>
+          </div>
+        </MDBFooter>
       </body>
     </div>
   );
